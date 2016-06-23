838543137db94f635821244c91f0f0f57279d107
Tran Van Tuan <tranvantuan.lak@gmail.com>
2016-06-23 17:26:26 +0700

    update src

--- itunes/run_populate.py
+++ itunes/run_populate.py
@@ -4,6 +4,6 @@
 import sys
 import os, os.path
 
-cmd = "../../../store-listing-toolkit/populate-v2.py metadata -platform iOS -prj-path . -sheet 1U7kv0k3myrcJtE1nOAdp7oDK_JxafVFnicabnu6ZBfE -customized-metadata-path ../../src/itunes/metadata"
+cmd = "../../../store-listing-toolkit/populate-v2.py metadata -platform iOS -prj-path . -sheet 1U7kv0k3myrcJtE1nOAdp7oDK_JxafVFnicabnu6ZBfE -customized-metadata-path ../src/itunes/metadata"
 print cmd
 os.system(cmd)
